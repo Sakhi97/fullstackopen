78a9f8e52f9ef05902e2206440494783e9230c53
Sakhi97 <sakhi250397@gmail.com>
2024-01-22 16:25:40 +0200

    2.8 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,34 +2,45 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
   const [alertMessage, setAlertMessage] = useState('');
+
 
   const addPersona = (event) => {
     event.preventDefault();
 
     const isPersonExist = persons.some(person => person.name === newName);
-    
+
     if (isPersonExist) {
       setAlertMessage(`${newName} already exists in the phonebook!`);
-      alert(alertMessage)
+      alert(alertMessage);
     } else {
-      setPersons([...persons, { name: newName }]);
+      setPersons([...persons, { name: newName, number: newNumber }]);
       setNewName('');
+      setNewNumber('');
       setAlertMessage('');
     }
   };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPersona}>
         <div>
-          name: 
-          <input 
-            value={newName} 
-            onChange={(e) => setNewName(e.target.value)} 
+          name:
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
           />
         </div>
         <div>
@@ -37,9 +48,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map((person, index) => (
-          <div key={index}>{person.name}</div>
-        ))}
+      {persons.map((person, index) => (
+        <div key={index}>
+          {person.name} {person.number}
+        </div>
+      ))}
     </div>
   );
 };
